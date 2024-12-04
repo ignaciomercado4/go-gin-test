85a3c2b41a7c7c7d51d871bd666c067542ea00f2
Ignacio Mercado <igmercado46@gmail.com>
2024-12-04 16:09:35 -0300

    adds example code from gin docs

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello from main.go")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "I'm alive",
+		})
+	})
+
+	r.Run()
 }
