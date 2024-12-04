cb31f9c140590f5d60e06e534603f8ccf9368ebd
Ignacio Mercado <igmercado46@gmail.com>
2024-12-04 16:39:29 -0300

    tries out req params

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"math/rand"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,9 +14,40 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "I'm alive",
 		})
+	})
+
+	r.GET("/randomNumber", func(c *gin.Context) {
+
+		randomNumber := rand.Intn(1000) + 1
+
+		c.JSON(http.StatusOK, gin.H{
+			"randomNumber": randomNumber,
+		})
+	})
+
+	r.GET("/numberParam/:num", func(c *gin.Context) {
+
+		num, err := strconv.Atoi(c.Param("num"))
+		var isGreaterThanFive bool
+
+		if err != nil {
+			fmt.Println("Unable to convert num param to string")
+		}
+
+		if num > 5 {
+			isGreaterThanFive = true
+		} else {
+			isGreaterThanFive = false
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"numParam":          num,
+			"isGreaterThanFive": isGreaterThanFive,
+		})
+
 	})
 
 	r.Run()
